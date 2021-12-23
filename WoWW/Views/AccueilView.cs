5c39b850c2438ec13b9d64bb0a208b1e43b89c66
CaroCap <carolinecap.event@gmail.com>
2021-12-23 13:35:01 +0100

    Titre ascii Art

---
+++ WoWW/Views/AccueilView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoWW.Views
+{
+    internal class AccueilView
+    {
+        public static void AccueilMenu()
+        {
+            Console.WriteLine("#     #                                                 #     #                  #     #                #####    #   ");
+            Console.WriteLine("#  #  #  ####  #####  #      #####      ####  ######    #  #  #   ##   #####     #  #  #   ##   #####  #     #  ##   ");
+            Console.WriteLine("#  #  #  ####  #####  #      #####      ####  ######    #  #  #   ##   #####     #  #  #   ##   #####  #     #  ##   ");
+            Console.WriteLine("#  #  # #    # #    # #      #    #    #    # #         #  #  #  #  #  #    #    #  #  #  #  #  #    #       # # #   ");
+            Console.WriteLine("#  #  # #    # #    # #      #    #    #    # #         #  #  #  #  #  #    #    #  #  #  #  #  #    #       # # #   ");
+            Console.WriteLine("#  #  # #    # #    # #      #    #    #    # #####     #  #  # #    # #    #    #  #  # #    # #    #  #####    #   ");
+            Console.WriteLine("#  #  # #    # #####  #      #    #    #    # #         #  #  # ###### #####     #  #  # ###### #    # #         #   ");
+            Console.WriteLine("#  #  # #    # #   #  #      #    #    #    # #         #  #  # #    # #   #     #  #  # #    # #    # #         #   ");
+            Console.WriteLine("## ##   ####  #    # ###### #####      ####  #          ## ##  #    # #    #     ## ##  #    # #####  ####### #####  ");
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("Que faire ?");
+            Console.WriteLine("1. Créer un compte Player ?");
+            Console.WriteLine("2. Se connecter");
+            Console.ReadLine();
+        }
+    }
+}
